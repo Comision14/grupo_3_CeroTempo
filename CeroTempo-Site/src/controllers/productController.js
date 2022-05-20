b6aa75b9192e220932544c852eebb2cb7c3ca821
EricM <menaericdaniel@gmail.com>
2022-05-20 05:59:51 -0300

    delete old structure

--- CeroTempo-Site/src/controllers/productController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    pCart : (req, res) => {
-        res.render('products/productCart');
-    },
-    pDetail : (req, res) => {
-        res.render('products/productDetail');
-    }
-}
--- CeroTempo-Site/src/controllers/usersController.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    register : (req,res) => {
-        return res.render('users/register');
-    },
-    login : (req,res) => {
-        return res.render('users/login');
-    }
-}
